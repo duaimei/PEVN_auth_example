eee2f5324e0012e493fb86ee95c91ce2637828b0
duaimei <detwiler.amy@gmail.com>
2019-08-29 23:25:12 -0400

    auth hell

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const routes = require('express').Router();
-const secret = require('../config/client_secret.json')
+const secret = require('../config/secrets.json')
 const {google} = require('googleapis');
 
 
 const oauth2Client = new google.auth.OAuth2(
-  secret.web.client_id,
-  secret.web.client_secret,
-  secret.web.redirect_uris
+  secret.GOOGLE_CLIENT_ID,
+  secret.GOOGLE_CLIENT_SECRET,
+  ["http://localhost:8080/auth/google/callback"]
 );
 
 routes.get('/yess', (req, res) => {
@@ -17,9 +17,9 @@ routes.get('/yess', (req, res) => {
 routes.post('/auth/google', (req, res) => {
   console.log('auth google post req')
   oauth2Client.params = ["profile"]
-  console.log(req.body)
   oauth2Client.getToken(req.body.code, function (err, tokens) {
     const credentials = oauth2Client.setCredentials(tokens);
+    console.log(err)
   })
 })
 
